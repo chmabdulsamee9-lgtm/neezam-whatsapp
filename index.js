3c9ff5d046c26ce39aba17b0bfb19b142327a852
Abdul Samee <chmabdulsamee9@gmail.com>
2026-06-12 17:05:12 +0500

    fix: cors headers

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
